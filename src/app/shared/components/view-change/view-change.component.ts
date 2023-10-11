27db128a74df515c13c678617d591fb1cfdba880
manisha-backbase <108878362+manisha-backbase@users.noreply.github.com>
2023-10-11 13:05:30 +0200

    feature: created release details popup view (#8)
    * feature: created release details popup view
    
    * chore: fixed json

--- src/app/shared/components/view-change/view-change.component.ts
+++ src/app/shared/components/view-change/view-change.component.ts
@@ -12,5 +12,32 @@ import { ChangeType, ReleaseChanges } from 'src/app/shared/models/static.model';
   styleUrls: ['./view-change.component.scss'],
 })
 export class ViewChangeComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: ReleaseChanges) {}
+  data: ReleaseChanges;
+  currentIndex: number = 0;
+  isFirstElement: boolean = true;
+  isLastElement: boolean = false;
+  constructor(@Inject(MAT_DIALOG_DATA) public releaseData: any) {
+  }
+
+  ngOnInit(): void {
+
+    this.currentIndex = this.releaseData.dataSource.findIndex(element => element.id === this.releaseData.id);
+    this.data = this.releaseData.dataSource[this.currentIndex];
+    this.initDetailsNavigationButton();
+  }
+
+  getReleaseDetails(type: string) {
+    if (type === 'previous' && this.currentIndex > 0) {
+      this.currentIndex--;
+    } else if (type === 'next' && this.currentIndex < this.releaseData.dataSource.length - 1) {
+      this.currentIndex++;
+    }
+    this.initDetailsNavigationButton();
+    this.data = this.releaseData.dataSource[this.currentIndex];
+  }
+
+  initDetailsNavigationButton() {
+    this.isFirstElement = this.currentIndex === 0;
+    this.isLastElement = this.currentIndex === this.releaseData.dataSource.length - 1;
+  }
 }
