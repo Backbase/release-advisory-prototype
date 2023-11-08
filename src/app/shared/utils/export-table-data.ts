f6cf54806ea720b46a1b30c581e639532590a82f
ankitp_backbase <ankitp@backbase.com>
2023-11-08 14:56:30 +0100

    chore: fix excel writeFile code

--- src/app/shared/utils/export-table-data.ts
+++ src/app/shared/utils/export-table-data.ts
@@ -18,25 +18,35 @@ export class TableUtil {
   ) {
     let { sheetName, fileName } = getFileName();
 
+    let wb = null;
+
     if (exportOnlyVisibleRows) {
       let targetTableElm = document.getElementById(tableId);
 
-      let wb = XLSX.utils.table_to_book(targetTableElm, <XLSX.Table2SheetOpts>{
+      wb = XLSX.utils.table_to_book(targetTableElm, <XLSX.Table2SheetOpts>{
         sheet: sheetName,
       });
-
-      XLSX.writeFile(wb, `${fileName}.${format}`, {
-        bookType: format,
-      });
-
-      return;
+    } else {
+      // convert json to sheet and export
+      const ws = XLSX.utils.json_to_sheet(
+        jsonData.map((item) => ({
+          Calver: item.calVer,
+          Product: item.product,
+          Discipline: item.discipline,
+          'Change Type': item.changeType.name,
+          Version: item.version,
+          Title: item.title,
+          Description: item.description,
+        }))
+      );
+      wb = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws);
     }
 
-    // convert json to sheet and export
-    const ws = XLSX.utils.json_to_sheet(jsonData);
-    const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws);
-
+    if (!wb) {
+      console.error('No workbook created for table export.');
+      return;
+    }
     XLSX.writeFile(wb, `${fileName}.${format}`, {
       bookType: format,
     });
